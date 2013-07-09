188b6645847473e31f2ca50b6cc7d488c8481391
Alicja <acyganiewicz@gmail.com>
2013-07-09 12:34:59 +0200

    Colors of Chart changed to 'Forgive & Forget' palette

--- prototyp/js/charts_additional.js
+++ prototyp/js/charts_additional.js
@@ -2,9 +2,9 @@ var data_pop = {
 	labels: [2000,2001,2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010],
 	datasets : [
 		{
-			fillColor : "rgba(253,206,206,0.5)",
-			strokeColor : "rgba(168,192,192,1)",
-			pointColor : "rgba(168,192,192,1)",
+			fillColor : "rgba(74,24,62,0.5)",
+			strokeColor : "rgba(90,158,135,1)",
+			pointColor : "rgba(90,158,135,1)",
 			pointStrokeColor : "#fff",
 			data : [35,25,70,62,59,55,69,68, 70, 65, 72]
 		}
@@ -14,7 +14,7 @@ var data_pop = {
 var options = {
 				
 	//String - Colour of the scale line	
-	scaleLineColor : "rgba(133,167,167,.1)",
+	scaleLineColor : "rgba(67,63,97,.1)",
 	
 	//String - Scale label font declaration for the scale label
 	scaleFontFamily : "'Arial'",
@@ -26,7 +26,7 @@ var options = {
 	scaleFontStyle : "normal",
 	
 	//String - Scale label font colour	
-	scaleFontColor : "rgb(133,167,167)",	
+	scaleFontColor : "rgb(67,63,97)",	
 	
 	
 	//String - Colour of the grid lines

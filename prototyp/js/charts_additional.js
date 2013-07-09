a816816c5338e44df5750aaf54b578e6cccc6487
Alicja <acyganiewicz@gmail.com>
2013-07-09 12:14:06 +0200

    Fonts and styles modified in animal.html, added CHART of population years

---
+++ prototyp/js/charts_additional.js
@@ -0,0 +1,45 @@
+var data_pop = {
+	labels: [2000,2001,2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010],
+	datasets : [
+		{
+			fillColor : "rgba(253,206,206,0.5)",
+			strokeColor : "rgba(168,192,192,1)",
+			pointColor : "rgba(168,192,192,1)",
+			pointStrokeColor : "#fff",
+			data : [35,25,70,62,59,55,69,68, 70, 65, 72]
+		}
+	]
+};
+
+var options = {
+				
+	//String - Colour of the scale line	
+	scaleLineColor : "rgba(133,167,167,.1)",
+	
+	//String - Scale label font declaration for the scale label
+	scaleFontFamily : "'Arial'",
+	
+	//Number - Scale label font size in pixels	
+	scaleFontSize : 12,
+	
+	//String - Scale label font weight style	
+	scaleFontStyle : "normal",
+	
+	//String - Scale label font colour	
+	scaleFontColor : "rgb(133,167,167)",	
+	
+	
+	//String - Colour of the grid lines
+	scaleGridLineColor : "rgba(0,0,0,.05)",
+	
+	//Number - Pixel width of point dot stroke
+	pointDotStrokeWidth : 1,
+	
+	//Number - Number of animation steps
+	animationSteps : 200,
+	
+};
+
+
+var ctx_chart_population = document.getElementById('chart_population').getContext("2d");
+var chart_population = new Chart(ctx_chart_population).Line(data_pop, options);

2a4395e9e208811295baa377d263c6cb3355b792
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-07-23 13:49:29 +0200

    Prototype of taxonomic tree moved to rails

--- species_plus/app/controllers/static_pages_controller.rb
+++ species_plus/app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+
+	@@data = File.read("app/assets/javascripts/math_map_compact.json")
+
+	def math_map_compact
+	  render :json => @@data
+	end
+
   def home
   end
 

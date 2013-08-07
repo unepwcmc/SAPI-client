2ded69e66042a790265711a76b26650f0902a231
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-08-07 15:18:08 +0200

    Creature from full json list, param - name, prototype

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,4 +1,11 @@
 class SpeciesController < ApplicationController
+require 'json'
+
+  @@data = File.read("app/assets/javascripts/species_list.json")
+
   def creature
+    @creatures = JSON.parse(@@data)
+    @creature = @creatures.select {|c| c["full_name"] == params["name"] }
   end
+
 end

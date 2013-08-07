8e3efcacf25a761894a95251784fce21b8161c2b
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-08-07 19:25:47 +0200

    Listing taxonomy and English names for each creature

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -4,8 +4,9 @@ require 'json'
   @@data = File.read("app/assets/javascripts/species_list.json")
 
   def creature
+    @tax = ["kingdom_name", "phylum_name", "class_name", "order_name", "family_name", "genus_name", "species_name", "full_name", "english_names", "polish_names"]
     @creatures = JSON.parse(@@data)
-    @creature = @creatures.select {|c| c["full_name"] == params["name"] }.shift
+    @creature = @creatures.select {|c| c["full_name"] == params["name"] }.shift.select{|k,v| @tax.include? k}
   end
 
 end

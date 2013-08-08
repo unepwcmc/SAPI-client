573e8e1e71ae8e2cba0ec7b606358a2d659d1ffd
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-08-08 11:19:34 +0200

    Cites appendix added to list of creature specification

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -4,7 +4,7 @@ require 'json'
   @@data = File.read("app/assets/javascripts/species_list.json")
 
   def creature
-    @tax = ["kingdom_name", "phylum_name", "class_name", "order_name", "family_name", "genus_name", "species_name", "full_name", "english_names", "polish_names"]
+    @tax = ["kingdom_name", "phylum_name", "class_name", "order_name", "family_name", "genus_name", "species_name", "full_name", "english_names", "polish_names", "current_listing"]
     @creatures = JSON.parse(@@data)
     @creature = @creatures.select {|c| c["full_name"] == params["name"] }.shift.select{|k,v| @tax.include? k}
   end

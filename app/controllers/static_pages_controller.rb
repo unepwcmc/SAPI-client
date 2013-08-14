2162546a65d75d8fedab8b69fb161efa0cb1ce63
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-08-14 12:20:53 +0200

    Reading species from json file

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,14 @@
 class StaticPagesController < ApplicationController
 
 	@@data = File.read("app/assets/javascripts/temp.json")
+  @@polish_creatures = File.read("app/assets/javascripts/species_list.json")
 
 	def math_map_compact
 	  render :json => @@data
 	end
 
   def home
+    gon.creatures = JSON.parse(@@polish_creatures)
   end
 
   def help

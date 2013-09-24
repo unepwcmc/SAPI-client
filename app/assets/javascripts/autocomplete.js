f6b1b9a6e21dc48753a7356f2281811008be04d5
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-09-24 11:33:11 +0200

    highlighting searching term

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -3,7 +3,22 @@ $(function() {
     $.each(gon.creatures, function (index, value){
       Tags.push(value['full_name']);
     });
+
+    $.extend( $.ui.autocomplete.prototype, {
+        _renderItem: function( ul, item ) {
+            var term = this.element.val(),
+                html = item.label.replace( term, "<b>$&</b>" ),
+                upper = term.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); });
+            html = html.replace( upper, "<b>$&</b>");
+            return $( "<li></li>" )
+                .data( "item.autocomplete", item )
+                .append( $("<a></a>").html(html) )
+                .appendTo( ul );
+        }
+    });
+
     $( "#species_name" ).autocomplete({
-      source: Tags
+      source: Tags,
+      delay: 0
     });
   });
